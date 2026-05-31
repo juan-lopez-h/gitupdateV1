83bbc5f75ccfb1fa665b2c9bf35b2807cc81c5e8
juancho <murdersinc23@gmail.com>
2026-05-31 17:16:18 -0500

    scrip 1 prueba

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in $(seq -w 1 27)
+do
+    echo "Actividad $i" >> historial.txt
+
+    git add historial.txt
+
+    HORA=$((RANDOM % 8 + 8))
+    MIN=$((RANDOM % 60))
+
+    FECHA="2026-05-$i $(printf "%02d:%02d:00" $HORA $MIN)"
+
+    GIT_AUTHOR_DATE="$FECHA" \
+    GIT_COMMITTER_DATE="$FECHA" \
+    git commit -m "Actividad $i"
+done
